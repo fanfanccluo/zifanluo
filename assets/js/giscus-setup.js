435db1360dc2a740e1489d4620f3d8d6d0e3f612
fanfanccluo <155929239+fanfanccluo@users.noreply.github.com>
2026-03-16 19:19:17 +0000

    Deploying to gh-pages from @ fanfanccluo/fanfanccluo.github.io@d08ea0f6908be505736c3aca3cf16bddf496ed0b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "fanfanccluo/zifanluo",
+    "data-repo": "fanfanccluo/fanfanccluo.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
